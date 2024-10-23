f704db68fa9c9a41142430a5c295025e5943db01
Stefan Göllner <goellnor@gmail.com>
2024-10-23 17:31:19 +0200

    Initial Commit

---
+++ Fetch Menu BarTests/Fetch_Menu_BarTests.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_Menu_BarTests.swift
+//  Fetch Menu BarTests
+//
+//  Created by Stefan Göllner on 23.10.24.
+//
+
+import Testing
+@testable import Fetch_Menu_Bar
+
+struct Fetch_Menu_BarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
